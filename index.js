df4e9cfbc482f5ff4f696596ef4468d33eeefa37
ed4354 <eddli.sac@gmail.com>
2019-03-12 21:43:29 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hey: 'Amy' });
+  res.send({ Wassup: '( ▀ ͜͞ʖ▀) (ツ) ( ͡° ͜ʖ ͡°) ' });
 });
 
 const PORT = process.env.PORT || 5000;
